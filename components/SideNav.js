5c7a226a856e47c13ac6c73c14213fcd79af4140
Harry Keightley <harrykeightley@outlook.com>
2022-02-19 11:58:27 +1000

    sidebar update

--- components/SideNav.js
+++ components/SideNav.js
@@ -6,10 +6,15 @@ export default function SideNav({ posts }) {
     <div className="w-60 h-full mt-4">
       <div className="sticky top-0 h-full">
         <p className="mb-4 font-bold">Course Outline</p>
-        <ul className="flex flex-col space-y-2 border rounded-lg p-2 bg-slate-200">
+        <ul
+          className="grid grid-cols-1 border shadow-md 
+        shadow-slate-300 rounded-lg px-2 bg-slate-200 divide-y divide-slate-300"
+        >
           {posts.map((post, index) => (
             <Link key={post.title} href={`/course/${post.slug}`} passHref>
-              <li className="cursor-pointer text-blue-500">{post.title}</li>
+              <li className="cursor-pointer text-blue-500 py-2">
+                {post.order} {post.title}
+              </li>
             </Link>
           ))}
         </ul>
